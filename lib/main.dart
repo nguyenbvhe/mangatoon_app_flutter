6cc3b904036e5b7ce44dc7e40813c47c351203df
nguyenbvhe <buivannguyen1312@gmail.com>
2026-02-24 15:16:30 +0700

    Update main.dart
    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'login/auth_wrapper.dart';    
+import 'home/home_page.dart'; // File chứa giao diện truyện tranh
 import 'login/login_screen.dart';
 import 'login/register_screen.dart';
+import 'login/auth_wrapper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,59 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Manga Auth',
+      title: 'MangaToon Clone',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'Roboto',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF8E79D1),
-          primary: const Color(0xFF8E79D1),
-        ),
-        // Bạn có thể thêm scaffoldBackgroundColor nếu muốn
-        // scaffoldBackgroundColor: Colors.transparent,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF8E79D1)),
       ),
-      home: const AuthNavigationCenter(),
+      // THAY ĐỔI TẠI ĐÂY: Màn hình Home sẽ hiện ra đầu tiên
+      home: const HomePage(), 
     );
-  }
-}
-
-class AuthNavigationCenter extends StatefulWidget {
-  const AuthNavigationCenter({super.key});
-
-  @override
-  State<AuthNavigationCenter> createState() => _AuthNavigationCenterState();
-}
-
-class _AuthNavigationCenterState extends State<AuthNavigationCenter> {
-  /// 'login' | 'register'
-  String _currentView = 'login';
-
-  void _navigateTo(String view) {
-    setState(() => _currentView = view);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AuthWrapper(
-      child: _buildCurrentBody(),
-    );
-  }
-
-  Widget _buildCurrentBody() {
-    switch (_currentView) {
-      case 'register':
-        return RegisterScreen(
-          onNavigateToLogin: () => _navigateTo('login'),
-        );
-
-      case 'login':
-      default:
-        return LoginScreen(
-          // Nếu LoginScreen không thực sự dùng onBack → có thể bỏ tham số này
-          onBack: () {}, 
-          onNavigateToRegister: () => _navigateTo('register'),
-        );
-    }
   }
 }
